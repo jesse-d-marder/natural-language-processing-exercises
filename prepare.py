f5ca241f53da0bf3e3d7e7bd59cf24871ca51b6b
Jesse Marder <jesse.d.marder@gmail.com>
2022-05-12 14:27:31 -0500

    Update file

--- prepare.py
+++ prepare.py
@@ -7,6 +7,7 @@ from nltk.tokenize.toktok import ToktokTokenizer
 from nltk.corpus import stopwords
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 import acquire
 
@@ -107,3 +108,19 @@ def create_prepared_blog_df(extra_words =[], exclude_words = []):
     return prepare_df(codeup_df, extra_words, exclude_words)
     
     
+def train_validate_test_split(df, target, seed=123):
+    '''
+    This function takes in a dataframe, the name of the target variable
+    (for stratification purposes), and an integer for a setting a seed
+    and splits the data into train, validate and test. 
+    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
+    original dataset, and train is .70*.80= 56% of the original dataset. 
+    The function returns, in this order, train, validate and test dataframes. 
+    '''
+    train_validate, test = train_test_split(df, test_size=0.2, 
+                                            random_state=seed, 
+                                            stratify=df[target])
+    train, validate = train_test_split(train_validate, test_size=0.3, 
+                                       random_state=seed,
+                                       stratify=train_validate[target])
+    return train, validate, test
